0c9500b2666425294bd90b1519debd6f14d035c6
Akshay Dere <akshaydere3204@gmail.com>
2025-03-10 22:31:49 +0530

    10/03/2025 - Updated Changes Done

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        System.out.println("AKshay Dere Patil");
+
+    // Single Lina Commnet  -   //Printed my name in console
+        // Multi Line Comment as below -
+        /*akshay
+                dere
+                patil*/
+    }
+}
