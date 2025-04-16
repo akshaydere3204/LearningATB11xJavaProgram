10df59805a45c31b479483f9724e7c2c3931abcb
Akshay Dere <akshaydere3204@gmail.com>
2025-04-16 22:20:59 +0530

    14, 15th & 16 April Code Push

---
+++ src/ex_32_Collection_Framework_DSA/QUEUE/Lab259_LinkedList.java
@@ -0,0 +1,18 @@
+package ex_32_Collection_Framework_DSA.QUEUE;
+
+import java.util.*;
+
+public class Lab259_LinkedList {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList();
+        queue.add(6);
+        queue.add(1);
+        queue.add(8);
+
+        Iterator<Integer> iterator = queue.iterator();
+        while(iterator.hasNext()) {
+            Integer value = iterator.next();
+            System.out.println(value);
+        }
+    }
+}
---
+++ src/ex_32_Collection_Framework_DSA/QUEUE/Lab260_ARRAYDeque.java
@@ -0,0 +1,13 @@
+package ex_32_Collection_Framework_DSA.QUEUE;
+
+import java.util.*;
+
+public class Lab260_ARRAYDeque {
+    public static void main(String[] args) {
+        Deque<Integer> deck = new ArrayDeque();
+        deck.push(15);
+        deck.push(10);
+        System.out.println(deck);
+
+    }
+}
